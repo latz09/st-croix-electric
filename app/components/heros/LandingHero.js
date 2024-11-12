01bde48836b384febe74b5fe354c7a8286cc0c46
latz09 <85132328+latz09@users.noreply.github.com>
2024-11-12 12:19:24 -0600

    get started now button on home page

--- app/components/heros/LandingHero.js
+++ app/components/heros/LandingHero.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import DelayAppear from '../utils/animations/DelayAppear';
+import Link from 'next/link';
 
 const LandingHero = ({ image, heading, subheading, blurb }) => {
 	return (
@@ -20,11 +21,16 @@ const LandingHero = ({ image, heading, subheading, blurb }) => {
 
 			{/* Heading and Subheading */}
 			<div className='relative z-20 flex flex-col items-center justify-center h-full text-center text-light px-4 max-w-5xl mx-auto'>
-				<h1 className='text-4xl md:text-6xl font-black'>{heading}</h1>
+				<h1 className='text-4xl md:text-6xl font-black pb-2'>{heading}</h1>
 				<DelayAppear>
-					<p className='mt-4 text-xl md:text-3xl font-bold text-secondary'>
+					<p className='pt-2 text-xl md:text-3xl font-bold text-secondary border-t border-primary'>
 						{subheading}
 					</p>
+					<Link href='/contact-us'>
+						<button className='text-lg mt-8 px-8 py-4 rounded-sm border border-light text-light font-bold hover:text-dark hover:border-dark hover:scale-95 transition hover:bg-secondary transition-colors duration-700'>
+							Get Started Now
+						</button>
+					</Link>
 				</DelayAppear>
 			</div>
 		</div>
--- app/components/utils/modals/RequestQuoteNow.js
+++ app/components/utils/modals/RequestQuoteNow.js
@@ -1,40 +1,19 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { FiPhone, FiMail } from 'react-icons/fi';
-import { QuoteModalOverlay } from './ModalOverlay';
-import SubmitContactForm from '../../contact/SubmitContactForm';
 
-
-const RequestQuoteNow = async () => {
-	
-	const [isOpen, setIsOpen] = useState(false);
-
-	const handleOpen = () => setIsOpen(true);
-	const handleClose = () => setIsOpen(false);
-
-	// Use useEffect to add/remove the 'no-scroll' class to the body element
-	useEffect(() => {
-		if (isOpen) {
-			document.body.classList.add('no-scroll');
-		} else {
-			document.body.classList.remove('no-scroll');
-		}
-
-		// Cleanup when the component unmounts
-		return () => document.body.classList.remove('no-scroll');
-	}, [isOpen]);
-
+const RequestQuoteNow = () => {
 	return (
 		<>
 			<div className='bg-dark fixed bottom-0 left-0 right-0 z-50'>
 				<div className='max-w-3xl mx-auto  text-light flex justify-around gap-2 items-center p-4 shadow-lg  '>
-					<button
-						className='bg-secondary text-dark py-2 rounded-sm font-bold w-1/2 text-center'
-						onClick={handleOpen}
+					<Link
+						href='/contact-us'
+						className='bg-secondary w-1/2 text-center py-2 rounded-full '
 					>
-						Request Quote
-					</button>
+						<button className=' text-dark  font-bold '>Request Quote</button>
+					</Link>
 					<div className='w-1/2 flex justify-around text-3xl'>
 						<a href='tel:+6124309156' className='flex items-center gap-2'>
 							<FiPhone className='text-secondary' />
@@ -48,27 +27,8 @@ const RequestQuoteNow = async () => {
 					</div>
 				</div>
 			</div>
-
-			{/* Modal Overlay */}
-			<div className='z-50'>
-				<QuoteModalOverlay isOpen={isOpen} onClose={handleClose}>
-					<div>
-						<SubmitContactForm />
-						<div className='grid place-items-center w-full'>
-							<button
-								className='mt-8 bg-primary w-1/2 text-light py-1 px-3 rounded-full font-semibold'
-								onClick={handleClose}
-							>
-								Close
-							</button>
-						</div>
-					</div>
-				</QuoteModalOverlay>
-			</div>
 		</>
 	);
 };
 
 export default RequestQuoteNow;
-
-export const revalidate = 10;
