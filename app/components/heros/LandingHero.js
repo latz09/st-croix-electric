1e2669c2fdbf167a852dec06d6df8e1b5e0533c1
latz09 <85132328+latz09@users.noreply.github.com>
2024-11-15 14:21:27 -0600

    d

--- app/components/heros/LandingHero.js
+++ app/components/heros/LandingHero.js
@@ -21,13 +21,13 @@ const LandingHero = ({ image, heading, subheading, blurb }) => {
 
 			{/* Heading and Subheading */}
 			<div className='relative z-20 flex flex-col items-center justify-center h-full text-center text-light px-4 max-w-5xl mx-auto'>
-				<h1 className='text-4xl md:text-6xl font-black pb-2'>{heading}</h1>
+				<h1 className='text-3xl md:text-5xl font-black pb-2'>{heading}</h1>
 				<DelayAppear>
-					<p className='pt-2 text-xl md:text-3xl font-bold text-secondary border-t border-secondary'>
+					<p className='pt-2 text-lg md:text-2xl font-bold text-secondary border-t border-secondary'>
 						{subheading}
 					</p>
 					<Link href='/contact-us'>
-						<button className='text-lg mt-8 px-8 py-4 rounded-sm border border-light text-light font-bold hover:text-dark hover:border-dark hover:scale-95 transition hover:bg-secondary transition-colors duration-700'>
+						<button className='text-lg mt-8 px-8 py-4 rounded-sm border border-light text-light font-bold hover:text-dark hover:border-dark hover:scale-95 transition hover:bg-secondary  duration-700'>
 							Get Started Now
 						</button>
 					</Link>
