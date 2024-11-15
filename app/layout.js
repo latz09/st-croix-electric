4d9021ba60ddb2847fab52d9c875d24f068f7ec7
latz09 <85132328+latz09@users.noreply.github.com>
2024-11-15 14:37:15 -0600

    met description

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 		default: 'St. Croix Electric',
 		template: `%s | St. Croix Electric inc. `,
 	},
-	description: ``,
+	description: `St. Croix Electric offers trusted electrical solutions for historic homes and new builds. As a family-owned business with a legacy of excellence, we deliver precision, care, and innovative services tailored to your needs. Experience expertise you can rely on.`,
 	twitter: {
 		card: 'summary_large_image',
 	},
